a6abc0040219a346eb47f7451f67600e762265c2
Anna Broome <abroome@stanford.edu>
2023-07-05 16:56:31 -0700

    anna rugged error processing

--- tests/error_code_late_command_sweep.py
+++ tests/error_code_late_command_sweep.py
@@ -1,4 +1,3 @@
-import gpiozero
 import time
 import argparse
 import os
@@ -67,28 +66,30 @@ def test_with_pulse_rep_int(yaml_filename, pulse_rep_int, timeout_s=60*2, tmp_ya
     return {'file_prefix': file_prefix, 'pulse_rep_int': pulse_rep_int, 'n_errors': n_errors}
     #return {'file_prefix': "", 'pulse_rep_int': pulse_rep_int, 'n_errors': n_errors}
 
-expected_cwd = "/home/ubuntu/uhd_radar"
+#expected_cwd = "/home/thomas/Documents/StanfordGrad/RadioGlaciology/sdr"
+expected_cwd = "/home/radioglaciolgy/anna/uhd_radar"
 
 if __name__ == "__main__":
 
     # Check for correct working directory
     if os.getcwd() != expected_cwd:
-        print(f"This script should ONLY be run from {expected_cwd}. Detected CWD {os.getcwd()}")
-        error_and_quit()
+        raise Exception(f"This script should ONLY be run from {expected_cwd}. Detected CWD {os.getcwd()}")
 
     # Check if a YAML file was provided as a command line argument
     parser = argparse.ArgumentParser()
     parser.add_argument("yaml_file", nargs='?', default='config/default.yaml',
             help='Path to YAML configuration file')
+    parser.add_argument("--full_duplex", action='store_true',
+                        help='Calculate duty cycle for a full duplex transport layer. By default, assumes half duplex.')
     args = parser.parse_args()
     yaml_filename = args.yaml_file
 
+    yaml = YAML(typ='safe')
+    with open(yaml_filename) as stream:
+        config = yaml.load(stream)
+
     # Chirp generation
-    try:
-        generate_from_yaml_filename(yaml_filename)
-    except Exception as e:
-        print(e)
-        error_and_quit()
+    generate_from_yaml_filename(yaml_filename)
 
     # Compile UHD program
 
@@ -97,21 +98,52 @@ if __name__ == "__main__":
     run_and_fail_on_nonzero("make")
     os.chdir("../..")
 
-    # Run sweep
-    values = np.arange(500e-6, 4000e-6, 200e-6) #[1000e-6, 2000e-6, 3000e-6, 4000e-6, 5000e-6]
+    # Figure out a reasonable sweep range
+    if args.full_duplex:
+        active_time = max(config['CHIRP']['tx_duration'], config['CHIRP']['rx_duration'])
+        print(f"Full-duplex mode. Using active time of {active_time} seconds, which is the maximum of tx_duration and rx_duration")
+    else:
+        active_time = (config['CHIRP']['tx_duration'] + config['CHIRP']['rx_duration']) / 2
+        print(f"Half-duplex mode. Using active time of {active_time} seconds, which is the average of tx_duration and rx_duration")
+    
+    # Converters to show pulse_rep_int in terms of the effective duty cycle
+    # (averaged across TX and RX)
+    # pri is in milliseconds
+    def pri_to_duty(pri):
+        return 100 * active_time / (pri)
+    
+    def duty_to_pri(duty):
+        return 100 * active_time / (duty)
+
+    duty_cycles = np.arange(pri_to_duty(max(config['CHIRP']['tx_duration'], config['CHIRP']['rx_duration'])), 1.0, -2.5) # in percent
+    values = duty_to_pri(duty_cycles)
     results = {}
 
+    # Run sweep
     for v in values:
         results[v] = test_with_pulse_rep_int(yaml_filename, pulse_rep_int = float(v))
 
     for k, v in results.items():
         print(f"pulse_rep_int: {k} \tn_errors: {v['n_errors']} \tprefix: {v['file_prefix']}")
 
-    n_error_list = [results[v]['n_errors'] for v in values]
+    n_error_list = np.array([results[v]['n_errors'] for v in values])
+
+    import pickle
+    with open('test.pickle', 'wb') as f:
+        pickle.dump({'n_error_list': n_error_list, 'values': values, 'config': config}, f)
 
     fig, ax = plt.subplots(figsize=(10,6), facecolor='white')
-    ax.scatter(values * 1e3, n_error_list)
-    ax.set_xlabel('pulse_rep_int [ms]')
-    ax.set_ylabel('# of ERROR_CODE_LATE_COMMAND')
+    ax.scatter(duty_cycles, n_error_list / config['CHIRP']['num_pulses'] * 100)
+    ax.set_xlabel('Effective duty cycle [%]')
+    secax = ax.secondary_xaxis('top', functions=(duty_to_pri, pri_to_duty))
+    secax.set_xlabel('pulse_rep_int [microseconds]')
+    secax.set_xticks(duty_to_pri(ax.get_xticks()))
+    secax.set_xticklabels([f"{x*1e6:.2f} us" for x in secax.get_xticks()], rotation="vertical")
+    ax.set_ylim(0, 100)
+    ax.set_xlim(duty_cycles[0], duty_cycles[-1])
+
+    ax.set_ylabel('Percent of ERROR_CODE_LATE_COMMAND [%]')
+    ax.set_title(f"tx_duration: {config['CHIRP']['tx_duration']}, rx_duration: {config['CHIRP']['rx_duration']}, num_pulses: {config['CHIRP']['num_pulses']}")
     ax.grid()
+    fig.tight_layout()
     fig.savefig('error_code_late_command.png')
