db1f39dee9eb323585d39d8029372992d6086a6c
Anna Broome <abroome@stanford.edu>
2023-07-14 06:57:58 -0700

    added kaiser window with beta of 14

--- preprocessing/generate_chirp.py
+++ preprocessing/generate_chirp.py
@@ -39,6 +39,8 @@ def generate_chirp(config):
         chirp_complex = chirp_complex * np.blackman(chirp_complex.size)
     elif window == "hamming":
         chirp_complex = chirp_complex * np.hamming(chirp_complex.size)
+    elif window == "kaiser14":
+        chirp_complex = chirp_complex * np.kaiser(chirp_complex.size, 14.0)
     elif window != "rectangular":
         printf("[ERROR] Unrecognized window function '{window}'")
         return None, None
